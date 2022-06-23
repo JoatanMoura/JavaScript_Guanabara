5e2f4583286ef555576104611dbe84255af4ff26
JoatanMoura <jothah@gmail.com>
2022-06-23 16:58:20 -0300

    final do exercicio dia-tarde-noite

--- aula12ex/ex014/script.js
+++ aula12ex/ex014/script.js
@@ -1,1 +1,21 @@
-alert('Olá!')
+function carregar() {
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.getHours()
+    var hora = 15
+    msg.innerHTML = `Agora são ${hora} horas.`
+    if (hora >= 0 && hora < 12) {
+        img.src = 'fotomanha.png'
+        document.body.style.background = '#ffcf83'
+        // BOM DIA
+    } else if (hora >= 12 && hora < 18) {
+        img.src = 'fototarde.png'
+        document.body.style.background = '#cb4c00'
+        // BOA TARDE
+    } else {
+        img.src = 'fotonoite.png'
+        document.body.style.background = '#332a47'
+        // BOA NOITE
+    }
+}
