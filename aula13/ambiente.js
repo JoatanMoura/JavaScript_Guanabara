0f05cec2287dc133880c8b1233f76c5385195a5f
JoatanMoura <jothah@gmail.com>
2022-06-27 22:51:03 -0300

    estruturas de repeticao

---
+++ aula13/ambiente.js
@@ -0,0 +1,25 @@
+/*  EXECUTA O BLOCO PRIMEIRO E DEPOIS TESTA A CONDIÇÃO
+var c = 1
+do {
+    console.log('Tudo bem?')
+    c++
+} while (c <= 6)
+*/
+
+/* TESTA A CONDIÇÃO PRIMEIRO E DEPOIS EXECUTA O BLOCO
+var c = 1
+while (c <= 6) {
+    console.log('Tudo bem?')
+    c++
+}
+*/
+
+
+/*
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+*/
