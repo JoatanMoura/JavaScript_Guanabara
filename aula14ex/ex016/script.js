50bf521a3dce65de4d691b0916ced95cf4a6cd7e
JoatanMoura <jothah@gmail.com>
2022-07-05 15:01:30 -0300

    exercicio passo

--- aula14ex/ex016/script.js
+++ aula14ex/ex016/script.js
@@ -1,55 +1,64 @@
 function conta () {
-    var numIni = parseInt(document.getElementById('numIni').value)
-    var numFim = parseInt(document.getElementById('numFim').value)
-    var passo = parseInt(document.getElementById('passo').value)
-    //var contador = contador + passo
-    var valIni
-    var valFim
-    var somou
-    var lista
-    var lista2
-    var res = document.getElementById('res')
+    let numIni = parseInt(document.getElementById('numIni').value)
+    let numFim = parseInt(document.getElementById('numFim').value)
+    let passo = parseInt(document.getElementById('passo').value)
+    let res = document.getElementById('res')
     
     if (isNaN(numIni) || isNaN(numFim) || isNaN(passo)) {
         window.alert('Nenhum dos campos pode ser vazio.')
-    } else {
+    /*} else if (numIni < numFim) {
         valFim = numIni
-        //lista = valFim
-
-        do {
-
-            somou = valFim + passo
-            lista2 = ' - '
-           lista = valFim + lista2 + somou
-
-        } while (valFim <= numFim)
-    }
-
-    //res.innerHTML = `Contando: <br>${numIni}<br>${numFim}<br>${passo}`
-    res.innerHTML = lista
-}
-
-function conta2 () {
-    var numIni = parseInt(document.getElementById('numIni').value)
-    var numFim = parseInt(document.getElementById('numFim').value)
-    var passo = parseInt(document.getElementById('passo').value)
-
-    var res = document.getElementById('res')
-    
-    if (isNaN(numIni) || isNaN(numFim) || isNaN(passo)) {
-        window.alert('Nenhum dos campos pode ser vazio.')
-    } else {
-        valFim = numIni
-        lista = valFim + ' - '
-        valFim = valFim + passo
+        lista = valFim + ` \u{1F449}`
+        valFim += passo
         while (valFim <= numFim) {
-            //window.alert(lista)
-            lista = lista + valFim + ' - '
-            valFim = valFim + passo
-        } 
-    }
-    lista = lista + ' # '
+            lista = lista + valFim + ` \u{1F449}`
+            valFim += passo
+        }*/
+    } else if (numFim < numIni) {
+            valFim = numFim
+            lista = valFim + ` \u{1F449}`
+            valFim -= passo
+            while (valFim >= numFim) {
+                lista = lista + valFim + ` \u{1F449}`
+                valFim -= passo
+            }
+    } 
+    lista = lista + ` \u{1F3C1}`
     
     res.innerHTML = `Contando: <br>${lista}`
     //res.innerHTML = lista
 }
+
+/*
+function conta () {
+    let numIni = parseInt(document.getElementById('numIni').value)
+    let numFim = parseInt(document.getElementById('numFim').value)
+    let passo = parseInt(document.getElementById('passo').value)
+    let res = document.getElementById('res')
+    
+    if (isNaN(numIni) || isNaN(numFim) || isNaN(passo)) {
+        window.alert('Nenhum dos campos pode ser vazio.')
+    } else if (numIni < numFim) {
+        valFim = numIni
+        lista = valFim + ` \u{1F449}`
+        valFim += valFim + passo
+        while (valFim <= numFim) {
+            lista = lista + valFim + ` \u{1F449}`
+            valFim = valFim + passo
+        }
+    } else if (numFim < numIni) {
+            valFim = numFim
+            lista = valFim + ` \u{1F449}`
+            valFim = valFim - passo
+            while (valFim >= numFim) {
+                lista = lista + valFim + ` \u{1F449}`
+                valFim = valFim - passo
+            }
+    } 
+    lista = lista + ` \u{1F3C1}`
+    
+    res.innerHTML = `Contando: <br>${lista}`
+    //res.innerHTML = lista
+}
+
+*/
